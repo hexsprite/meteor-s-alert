2ebc647281bf9b657c4082707bca7faf55ded47d
Jordan Baker <jbb@scryent.com>
2025-12-10 10:40:25 -0500

    feat: Meteor 3 compatibility updates

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     'summary': 'Simple and fancy notifications / alerts / errors for Meteor',
-    'version': '3.2.0',
+    'version': '3.2.1',
     'git': 'https://github.com/juliancwirko/meteor-s-alert.git',
     'name': 'juliancwirko:s-alert'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('METEOR@1.1.0.3');
+    api.versionsFrom('3.0');
     api.use('mongo');
     api.use('templating');
-    api.use('ui');
+    api.use('blaze');
     api.use('underscore');
-    api.use(['jquery', 'session'], ['client']);
+    api.use(['session'], ['client']);
     api.addFiles([
         'client/s-alert.js',
         'client/s-alert-collection.js',
@@ -20,17 +20,5 @@ Package.onUse(function (api) {
         'client/s-alert-template.js'
     ], 'client');
     api.export('sAlert', ['client']);
-});
-
-Package.onTest(function (api) {
-    api.versionsFrom('METEOR@1.1.0.3');
-    api.use(['jquery'], ['client']);
-    api.use([
-        'templating',
-        'juliancwirko:s-alert@3.2.0',
-        'practicalmeteor:mocha@2.4.5_6',
-        'practicalmeteor:chai@2.1.0_1'
-    ]);
-    api.addFiles(['tests/s-alert-test.js'], 'client');
 });
 
